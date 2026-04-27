2aab5b279df784299b1e1f63749071bf5b9db51a
evellyn severo <evellyn.severo@zup.com.br>
2026-04-27 10:39:32 -0300

    feat: Criação do DTO Response

---
+++ src/main/java/com/example/academyfit/UsuarioResponseDTO.java
@@ -0,0 +1,11 @@
+package com.example.academyfit;
+
+import java.time.LocalDateTime;
+
+public class UsuarioResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private TrainingLevel trainingLevel;
+    private LocalDateTime createdAt;
+}
