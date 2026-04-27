ec6d2e33f846338d2bbebfe5123032ae2b32590c
evellyn severo <evellyn.severo@zup.com.br>
2026-04-27 14:53:47 -0300

    chore: movido para a raiz do projeto

--- src/main/java/com/example/academyfit/controller/AcademyfitApplication.java
+++ src/main/java/com/example/academyfit/controller/AcademyfitApplication.java
@@ -1,4 +1,4 @@
-package com.example.academyfit;
+package com.example.academyfit.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
