ea6db036f082d5a57b7231f121bb4d8573459738
evellyn severo <evellyn.severo@zup.com.br>
2026-04-27 10:28:50 -0300

    feat:Meu primeiro Commit

---
+++ src/main/java/com/example/academyfit/AcademyfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.academyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyfitApplication.class, args);
+	}
+
+}
