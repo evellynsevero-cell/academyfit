88a0e7152d0686c353a3105d87b74f907d5aa654
evellyn severo <evellyn.severo@zup.com.br>
2026-04-27 12:23:19 -0300

    feat: Atualizando a classe UsuarioResponseDTO

--- src/main/java/com/example/academyfit/UsuarioResponseDTO.java
+++ src/main/java/com/example/academyfit/UsuarioResponseDTO.java
@@ -8,4 +8,44 @@ public class UsuarioResponseDTO {
     private String email;
     private TrainingLevel trainingLevel;
     private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TrainingLevel getTrainingLevel() {
+        return trainingLevel;
+    }
+
+    public void setTrainingLevel(TrainingLevel trainingLevel) {
+        this.trainingLevel = trainingLevel;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
