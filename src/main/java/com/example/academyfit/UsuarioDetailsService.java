5d1988daa4676b41b31af7abc595524f57bbe5e6
evellyn severo <evellyn.severo@zup.com.br>
2026-04-27 12:36:05 -0300

    feat: Implementando UsuarioDetailsService.

---
+++ src/main/java/com/example/academyfit/UsuarioDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.academyfit;
+
+import com.example.academyfit.Usuario;
+import com.example.academyfit.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UsuarioDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getEmail(), usuario.getPassword(), new ArrayList<>()
+        );
+    }
+}
