a38610efea3292ecc0fe0441c47949cdf0c9fd5e
evellyn severo <evellyn.severo@zup.com.br>
2026-04-28 09:55:55 -0300

    chore: arquivo atualizado e movido para para pasta

---
+++ src/main/java/com/example/academyfit/controller/UsuarioController.java
@@ -0,0 +1,67 @@
+package com.example.academyfit.controller;
+
+import com.example.academyfit.dominio.usuario.exception.EmailAlreadyExistsException;
+import com.example.academyfit.dominio.usuario.dto.UsuarioRequestDTO;
+import com.example.academyfit.dominio.usuario.dto.UsuarioResponseDTO;
+import com.example.academyfit.dominio.usuario.model.Usuario;
+import com.example.academyfit.dominio.usuario.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService userService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody UsuarioRequestDTO dto) {
+        try {
+            UsuarioResponseDTO response = userService.createUsuario(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (EmailAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+
+        }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarTodos() {
+        return null;
+    }
+
+    {
+        List<UsuarioResponseDTO> usuarios = userService.listarTodos();
+        if (usuarios.isEmpty()) {
+            ResponseEntity<Object> build = ResponseEntity.noContent().build();
+    }
+        ResponseEntity<List<UsuarioResponseDTO>> ok = ResponseEntity.ok(usuarios);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        try {
+            userService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizar(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO request) {
+        try {
+            UsuarioResponseDTO response = userService.atualizar(id, request);
+            return ResponseEntity.ok(response);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
+
+
