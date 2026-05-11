8046186ecbf4c4aa833e2a2ea2398108a823a766
Anna Jullia <anna.areas@zup.com.br>
2026-05-11 09:58:31 -0300

    feat: configurar do script de carga de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO musculos (name, name_en) VALUES ('Peitoral maior', 'Chest');
+INSERT INTO musculos (name, name_en) VALUES ('Anterior deltoide', 'Shoulders');
+INSERT INTO musculos (name, name_en) VALUES ('Quadríceps', 'Quads');
+INSERT INTO musculos (name, name_en) VALUES ('Bíceps', 'Biceps');
+INSERT INTO musculos (name, name_en) VALUES ('Tríceps', 'Triceps');
+
+INSERT INTO equipamentos (name) VALUES ('Barbell');
+INSERT INTO equipamentos (name) VALUES ('Bench');
+INSERT INTO equipamentos (name) VALUES ('Dumbbell');
+INSERT INTO equipamentos (name) VALUES ('Pull-up bar');
+INSERT INTO equipamentos (name) VALUES ('Bodyweight');
